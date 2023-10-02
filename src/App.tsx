9da29ab94c9dc1f8b8758fdcea0221b55af2631d
Thanasit Suwanposri <41180013+men1164@users.noreply.github.com>
2023-10-02 17:32:51 +0700

    feat: blog

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
+import BlogLists from './components/BlogLists'
 import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <BlogLists />
     </div>
   )
 }
